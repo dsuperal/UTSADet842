43103198ec0b1b3ae9342ad21489c2a1ffc966b1
Your Name <dsuperal@trinity.edu>
2026-08-13 07:29:46 -0500

    nav bar fixed

--- assets/js/navbar_handler.js
+++ assets/js/navbar_handler.js
@@ -1,118 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const navContainer = document.getElementById('navbar-dropdown');
 
+    // 1. Fetch the JSON file
     fetch('assets/js/navbar_items.json')
         .then(response => {
-
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-
-            return response.json();
-
+            return response.json(); // Parse the JSON data
         })
-
         .then(navData => {
-
+            // 2. The data is ready, now build the HTML
             let htmlContent = '';
 
             navData.forEach(item => {
-
                 if (item.type === 'link') {
-
                     htmlContent += `
                         <li class="nav-item">
-                            <a class="nav-link" href="${item.url}">
-                                ${item.label}
-                            </a>
-                        </li>
-                    `;
-
-                }
-
-                else if (item.type === 'dropdown') {
-
+                            <a class="nav-link" href="${item.url}">${item.label}</a>
+                        </li>`;
+                        
+                } else if (item.type === 'dropdown') {
                     htmlContent += `
                         <li class="nav-item dropdown">
-
-                            <a
-                                class="nav-link dropdown-toggle"
-                                href="#"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false">
-
-                                ${item.label}
-
-                            </a>
-
-                            <ul class="dropdown-menu dropdown-menu-lg-end">
-                    `;
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">${item.label}</a>
+                            <ul class="dropdown-menu dropdown-menu-lg-end">`;
 
                     item.items.forEach(subItem => {
-
                         if (subItem.type === 'link') {
-
-                            htmlContent += `
-                                <li>
-                                    <a
-                                        class="dropdown-item"
-                                        href="${subItem.url}">
-                                        ${subItem.label}
-                                    </a>
-                                </li>
-                            `;
-
+                            htmlContent += `<li><a class="dropdown-item" href="${subItem.url}">${subItem.label}</a></li>`;
+                        } else if (subItem.type === 'divider') {
+                            htmlContent += `<li><hr class="dropdown-divider"></li>`;
                         }
-
-                        else if (subItem.type === 'divider') {
-
-                            htmlContent += `
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                            `;
-
-                        }
-
                     });
 
                     htmlContent += `
                             </ul>
-
-                        </li>
-                    `;
+                        </li>`;
                 }
-
             });
 
-
+            // 3. Inject the generated HTML into the container
             if (navContainer) {
-
                 navContainer.innerHTML = htmlContent;
-
-                // Initialize Bootstrap dropdowns
-                const dropdowns =
-                    navContainer.querySelectorAll(
-                        '[data-bs-toggle="dropdown"]'
-                    );
-
-                dropdowns.forEach(dropdown => {
-                    new bootstrap.Dropdown(dropdown);
-                });
-
             }
-
         })
-
         .catch(error => {
-
-            console.error(
-                'Error loading the navbar data:',
-                error
-            );
-
+            console.error('Error loading the navbar data:', error);
         });
-
 });
